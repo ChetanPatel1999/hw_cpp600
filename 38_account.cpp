dd7b28e5b568a983a0bb023be8f495349100c099
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-11-13 18:16:18 +0530

    13/11

--- 38_account.cpp
+++ 38_account.cpp
@@ -42,6 +42,120 @@ public:
             cout << "\nplease frist create account then display\n\n";
         }
     }
+
+    static void displayAccount(account ac[], int index)
+    {
+        if (index > 0)
+        {
+            int i, f = 0;
+            string number;
+            cout << "\nenter account number : ";
+            cin >> number;
+            for (i = 0; i < index; i++)
+            {
+                if (number == ac[i].acNo)
+                {
+                    f = 1;
+                    cout << "\naccount info : " << endl;
+                    cout << "account name " << ac[i].acName << endl;
+                    cout << "account number " << ac[i].acNo << endl;
+                    cout << "account balance " << ac[i].balance << "\n\n";
+                }
+            }
+            if (f == 0)
+            {
+                cout << "\n wrong account number\n"
+                     << endl;
+            }
+        }
+        else
+        {
+            cout << "\nplease frist create account then display\n\n";
+        }
+    }
+
+    static void creaditamount(account ac[], int index)
+    {
+        if (index > 0)
+        {
+            int i, f = 0, amt;
+            string number;
+            cout << "\nenter account number : ";
+            cin >> number;
+            cout << "enter amount to credit : ";
+            cin >> amt;
+            for (i = 0; i < index; i++)
+            {
+                if (number == ac[i].acNo)
+                {
+                    f = 1;
+                    ac[i].balance = ac[i].balance + amt;
+                    bankBalance = bankBalance + amt;
+                    cout << "\n"
+                         << amt << " amount is credit succefully\n\n";
+                }
+            }
+            if (f == 0)
+            {
+                cout << "\n wrong account number\n"
+                     << endl;
+            }
+        }
+        else
+        {
+            cout << "\nplease frist create account then creadit amount\n\n";
+        }
+    }
+
+    static void debitamount(account ac[], int index)
+    {
+        if (index > 0)
+        {
+            int i, f = 0, amt;
+            string number;
+            cout << "\nenter account number : ";
+            cin >> number;
+            cout << "enter amount to debit : ";
+            cin >> amt;
+            for (i = 0; i < index; i++)
+            {
+                if (number == ac[i].acNo)
+                {
+                    f = 1;
+                    if (amt <= ac[i].balance)
+                    {
+                        ac[i].balance = ac[i].balance - amt;
+                        bankBalance = bankBalance - amt;
+                        cout << "\n"
+                             << amt << " amount is debit succefully\n\n";
+                    }
+                    else
+                    {
+                        cout << "\n you have not sufficient balace" << endl;
+                        cout << "you have only " << ac[i].balance << endl;
+                        cout << "and you try to  debit " << amt << endl
+                             << endl;
+                    }
+                }
+            }
+            if (f == 0)
+            {
+                cout << "\n wrong account number\n"
+                     << endl;
+            }
+        }
+        else
+        {
+            cout << "\nplease frist create account then debit amount\n\n";
+        }
+    }
+    static void displayTotalBankBalance()
+    {
+        cout << "\nbank name : " << bankName << endl;
+        cout << "total account in bank : " << totalAccount << endl;
+        cout << "total bank balance  : " << bankBalance << endl
+             << endl;
+    }
 };
 int account::bankBalance = 0;
 string account::bankName = "HDFC";
@@ -68,22 +182,22 @@ int main()
         {
         case 1:
             account::creatAccount(ac, index);
-            index++;
+            index++; // 2
             break;
         case 2:
             account::displayAccounts(ac, index);
             break;
         case 3:
-            cout << "call display individual account method" << endl;
+            account::displayAccount(ac, index);
             break;
         case 4:
-            cout << "call creadit amount method" << endl;
+            account::creaditamount(ac, index);
             break;
         case 5:
-            cout << "call debit amount method" << endl;
+            account::debitamount(ac, index);
             break;
         case 6:
-            cout << "call totalamount display method" << endl;
+            account::displayTotalBankBalance();
             break;
         case 7:
             exit(0);
