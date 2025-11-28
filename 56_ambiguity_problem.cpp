8d8018612bfb7cdef20302dcb4ed8d770b12a960
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-11-28 17:59:13 +0530

    13/11

---
+++ 56_ambiguity_problem.cpp
@@ -0,0 +1,35 @@
+// multiple inheritance
+#include <iostream>
+using namespace std;
+class base1
+{
+public:
+    void greet()
+    {
+        cout << "good morning " << endl;
+    }
+};
+class base2
+{
+public:
+    void greet()
+    {
+        cout << "good afternoon" << endl;
+    }
+};
+
+class derived : public base1, public base2
+{
+public:
+    void greet()
+    {
+        // base2::greet();
+        base1::greet();
+    }
+};
+int main()
+{
+    derived d1;
+    d1.greet();
+    return 0;
+}
